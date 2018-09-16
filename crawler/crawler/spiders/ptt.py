e3826b24a415d19afb3e19632fdc55d06bdf1550
ZoeLiao <zoejoyuliao@gmail.com>
2018-09-16 11:19:05 +0800

    Initial scrapy

---
+++ crawler/crawler/spiders/ptt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+
+class PttSpider(scrapy.Spider):
+
+    name = 'ptt'
+
+    def start_requests(self):
+        # TBD
+
+    def parse(self, response):
+        # TBD
